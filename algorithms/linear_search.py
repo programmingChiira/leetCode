30ca84c23a4bde03ad0ae46047e890d813fb4b83
programmingChiira <programmingchiira@gmail.com>
2023-12-13 20:24:23 +0300

    my first understanding of lineear search

---
+++ algorithms/linear_search.py
@@ -0,0 +1,16 @@
+def linear_search(list, target):
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+
+def verify(index):
+    if index is not None:
+        print("The index was found at: ", index)
+    else:
+        print("The index was not found")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+result = linear_search(numbers, 12)
+verify(result)
