f0d63d61a80bbf660e0bb44c3ef37e9d01e05e90
programmingChiira <programmingchiira@gmail.com>
2023-12-15 15:13:22 +0300

    linear search learned

--- algorithms/linear_search.py
+++ algorithms/linear_search.py
@@ -12,5 +12,9 @@ def verify(index):
         print("The index was not found")
 
 numbers = [1,2,3,4,5,6,7,8,9,10]
+
 result = linear_search(numbers, 12)
+verify(result)
+
+result = linear_search(numbers, 6)
 verify(result)
