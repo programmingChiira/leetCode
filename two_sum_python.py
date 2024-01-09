fa6449b034e29629f6610c4d06bb932a78cfa764
DennisChiira <programmingchiira@gmail.com>
2024-01-09 14:04:34 +0300

    a solution of the two sum leetcode problem using python dictionary(hashMap)

---
+++ two_sum_python.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+        dictionary = {}
+        for i, num in enumerate(nums, start=0):
+            complement = target - num
+            if complement in dictionary:
+                return [dictionary[complement], i]
+            dictionary[num] = i
+        return None
+
+nums = [11, 2, 15, 7]
+target = 9
+result = twoSum(nums, target)
+print(result)
