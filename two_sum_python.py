e61ffb7b4e047f711cb0955b74cb2d5cbff941d9
DennisChiira <programmingchiira@gmail.com>
2024-01-09 14:21:38 +0300

    I get it, do you?

--- two_sum_python.py
+++ two_sum_python.py
@@ -1,13 +1,14 @@
 def twoSum(nums, target):
-        dictionary = {}
-        for i, num in enumerate(nums, start=0):
-            complement = target - num
-            if complement in dictionary:
-                return [dictionary[complement], i]
+    dictionary = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in dictionary:
+            return [dictionary[complement], i]
+        else:
             dictionary[num] = i
-        return None
+    return None
 
-nums = [11, 2, 15, 7]
+nums = [2, 7, 11, 15]
 target = 9
 result = twoSum(nums, target)
 print(result)
